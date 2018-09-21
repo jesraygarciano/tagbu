6baa23da2f7df08ffe5cbc3da737619588532b2d
Jesray Garciano <jesray.garciano@gmail.com>
2018-09-21 14:06:23 +0800

    Added favorite and unfavorite to posts controller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,8 +14,37 @@ class PostsController extends Controller
      */
 
     public function index(){
+
         $posts = Post::paginate(5);
 
         return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Favorite a post
+     * 
+     * @param Post $post
+     * @return response
+     */
+    public function favoritePost(Post $post)
+    {
+        
+        Auth::user()->favorites()->attach($post->id);
+
+        return back();
+    }
+
+    /**
+     * Unfavorite a post
+     * 
+     * @param Post $post
+     * @return response
+     */
+    public function unFavoritePost(Post $post){
+
+        Auth::user()->favorites()->detach($post->id);
+
+        return back();
+
     }
 }
