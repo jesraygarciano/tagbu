aa59eaa28f472c0fe498a9eb23267f64623e248d
Jesray Garciano <jesray.garciano@gmail.com>
2018-09-21 15:54:16 +0800

    Added my favorites view as well as Controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     //
+    /**
+     * Get all favorite posts by user
+     * 
+     * @return response
+     */
+
+     public function myFavorites(){
+
+        $myFavorites = Auth::users()->favorites;
+
+        return view('users.my_favorites', compact('myFavorites'));
+     }
 }
