5ecd348ab1aa63426bce7e5b8a0e98e4ff14af2a
Jesray Garciano <jesray.garciano@gmail.com>
2018-09-21 15:00:23 +0800

    Added Favorite component

---
+++ resources/js/components/Favorite.vue
@@ -0,0 +1,49 @@
+<template>
+    <span>
+        <a href="#" v-if="isFavorited" @click.prevent="unFavorite(post)">
+            <i class="fa fa-heart"></i>
+        </a>
+        <a href="#" v-else @click.prevent="favorite(post)">
+            <i class="fa fa-hear-0"></i>
+        </a>
+    </span>
+</template>
+
+<script>
+    export default{
+        props: [
+            'post',
+            'favorited'
+        ],
+
+        data: function(){
+            return {
+                isFavorited: '',
+            }
+        },
+
+        mounted() {
+            this.isFavorited = this.isFavorite ? true : false;
+        },
+
+        computed: {
+            isFavorite(){
+                return this.favorited;
+            },
+        },
+
+        methods: {
+            favorite(post){
+                axios.post('/favorite/' +post)
+                    .then(response => this.isFavorited = true)
+                    .catch(response => console.log(response.data));
+            },
+
+            unFavorite(post){
+                axios.post('/unfavorite/'+post)
+                    .then(reponse => this.isFavorited = false)
+                    .catch(reponse => console.log(response.data));
+            }
+        }
+    }
+</script>
